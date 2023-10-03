01bf37a515b3cfd2732ba19462df062618dd621e
Kilian Schnappinger <schnappingerkilian@gmail.com>
2023-10-03 08:46:20 +0200

    tried to implement algorithm to draw tree

--- src/ftest0.py
+++ src/ftest0.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from bigtree import dict_to_tree, postorder_iter
+from bigtree import dict_to_tree, postorder_iter, levelorder_iter
 import sys 
 import matplotlib.pyplot as plt 
 
@@ -10,6 +10,16 @@ class FTEST0:
         self.Lambda = Lambda
         self.k = k
         self.root = dict_to_tree(self.T)
+
+    def get_height(self): 
+        
+        height = 0
+        for key in self.T.keys():
+            pos = len(key.split("/"))
+            if pos > height:
+                height = pos
+
+        return height 
 
     def test_feasibility(self):
 
@@ -36,13 +46,62 @@ class FTEST0:
 
     def draw_algorithm(self): 
         
- 
         figure, axes = plt.subplots() 
-        cc = plt.Circle(( 0.5 , 0.5 ), 0.4 , alpha=0.1) 
+        height = self.get_height()
+        parent_dict = {}
         
+        for node in levelorder_iter(self.root): 
+            if node.is_root: 
+                cc = plt.Circle((2**height/2, 2*height), 0.5)
+                axes.add_artist( cc )
+                parent_dict[node.node_name] = (2**height/2, 2*height)
+                children = node.children
+                if len(children) == 1: 
+                    cc = plt.Circle((2**height/2, 2*height-1), 0.5)
+                    axes.add_artist( cc )
+                    parent_dict[children[0].node_name] = (2**height/4, 2*height-1)
+                else: 
+                    cc1 = plt.Circle((2**height/4, 2*height-1), 0.5)
+                    cc2 = plt.Circle(((3*(2**height))/4, 2*height-1), 0.5)
+                    axes.add_artist(cc1)
+                    axes.add_artist(cc2)
+                    parent_dict[children[0].node_name] = (2**height/4, 2*height-1)
+                    parent_dict[children[1].node_name] = ((3*(2**height))/4, 2*height-1)
+                
+            else: 
+                children = node.children
+                current_depth = len(node.path_name.split(node.sep))#
+                if len(children) == 0 or children is None: 
+                    continue
+                if len(children) == 1:
+                    par_tup = parent_dict[node.node_name]
+                    cc= plt.Circle((par_tup[0], par_tup[1]-1), 0.5) 
+                    parent_dict[children[0].node_name] = (par_tup[0], par_tup[1]-1)
+                    axes.add_artist(cc)
+                else: 
+                    par_tup = parent_dict[node.node_name]
+                    cc1 = plt.Circle((par_tup[0]+1/2, par_tup[1]-1), 0.5)
+                    cc1 = plt.Circle((par_tup[0]-1/2, par_tup[1]-1), 0.5)
+                    parent_dict[children[0].node_name] = (par_tup[0]+1/2, par_tup[1]-1)
+                    parent_dict[children[1].node_name] = (par_tup[0]-1/2, par_tup[1]-1)
+                    axes.add_artist(cc1)
+                    axes.add_artist(cc2)
+                
+        self.root.show(attr_list=["weight"])
         axes.set_aspect( 1 ) 
-        axes.add_artist( cc ) 
+        print(parent_dict)
+                
+            # for child in node.children: 
+
+            #     print(node)
+            #     cc = plt.Circle(( 0.5 , 0.5 ), 0.4 , alpha=0.1) 
+                
+            #     axes.set_aspect( 1 ) 
+            #     axes.add_artist( cc ) 
+        
         plt.title( 'Colored Circle' ) 
+        plt.xlim(0, 2**height)
+        plt.ylim(0, 2*height+1)
         plt.show()      
 
 
@@ -68,7 +127,7 @@ if __name__ == "__main__":
     ftest0 = FTEST0(T, Lambda, k)
     #print(ftest0.test_feasibility())
     ftest0.draw_algorithm()
-    
+    #print(ftest0.get_height())
     #root = dict_to_tree(path_dict)
 
     #root.show(attr_list=["weight"])
