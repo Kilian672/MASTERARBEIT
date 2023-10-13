3a504fb03aed2d420fb482ca5e70f110b48ccca1
Kilian Schnappinger <schnappingerkilian@gmail.com>
2023-10-13 13:46:35 +0200

    improved Radom tree class

--- src/random_tree.py
+++ src/random_tree.py
@@ -2,15 +2,27 @@ import random
 import networkx as nx 
 import matplotlib.pyplot as plt
 
+
 class RANDOMTREE: 
 
-    def __init__(self, height, max_noc, edge_weights=False):
+    def __init__(self, height, max_noc, edge_weights=True, colors=None):
         self.height = height
         self.max_noc = max_noc 
         self.edge_weights = edge_weights
+        self.colors = colors
         self.Tree = self.get_random_tree()
-        
+        if self.colors is not None: 
+            self.node_dict = self.color_tree()
+        else: 
+            self.node_dict = None
 
+    def color_tree(self): 
+        node_dict = {}
+        for node in self.Tree.nodes.keys():
+            node_dict[node] = {"color": random.randint(1,self.colors)}
+
+        return node_dict    
+        
     def get_random_tree(self): 
         
         tree_list = self.create_random_tree(self.height, self.max_noc)[0]
@@ -19,7 +31,10 @@ class RANDOMTREE:
         for node in tree_list.keys():
             if tree_list[node] != []: 
                 for child in tree_list[node]: 
-                    G.add_edge(node, child, weight=random.randint(1,10))
+                    if self.edge_weights: 
+                        G.add_edge(node, child, weight=random.randint(1,10))
+                    else: 
+                        G.add_edge(node, child)
 
         return G
 
@@ -47,7 +62,7 @@ class RANDOMTREE:
             while new_nodes == []: 
                 for node in current_nodes: 
                     max_index = max(tree.keys())
-                    random_list = list(range(max_index+1, max_index+random.randint(1,max_noc)))
+                    random_list = list(range(max_index+1, max_index+1+random.randint(1,max_noc)))
                     tree[node] = random_list
                     for index in random_list: 
                         tree[index] = []
@@ -57,13 +72,20 @@ class RANDOMTREE:
     
     def draw_tree(self): 
         
-        edge_labels = {(u_e, v_e): e_weight for u_e, v_e, e_weight in self.Tree.edges.data('weight')}
-        
-        
         pos = nx.spring_layout(self.Tree)
-        nx.draw_networkx_nodes(self.Tree, pos, node_size = 500)
+    
+        if self.node_dict is not None: 
+            cols = [self.node_dict[node]["color"] for node in self.node_dict.keys()]
+            nx.draw_networkx_nodes(self.Tree, pos, node_size = 500, node_color = cols)
+        else: 
+            nx.draw_networkx_nodes(self.Tree, pos, node_size = 500)
         nx.draw_networkx_edges(self.Tree, pos, arrows=False, label="weight")
-        nx.draw_networkx_edge_labels(self.Tree, pos, edge_labels= edge_labels)
+
+        # add labels to edges 
+        if self.edge_weights: 
+            edge_labels = {(u_e, v_e): e_weight for u_e, v_e, e_weight in self.Tree.edges.data('weight')}
+            nx.draw_networkx_edge_labels(self.Tree, pos, edge_labels= edge_labels)
+
         nx.draw_networkx_labels(self.Tree, pos)
         plt.show()
 
@@ -71,5 +93,5 @@ class RANDOMTREE:
 
 if __name__ == "__main__": 
 
-    random_tree = RANDOMTREE(5, 3)
+    random_tree = RANDOMTREE(3, 4, edge_weights=False, colors=2)
     random_tree.draw_tree()
