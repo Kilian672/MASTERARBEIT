9a2444b4c58a2f3ffa8944226ff4483c9f2e58ce
Kilian Schnappinger <schnappingerkilian@gmail.com>
2023-10-15 22:02:35 +0200

    made several changes in both classes

--- src/random_tree.py
+++ src/random_tree.py
@@ -4,53 +4,104 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class RANDOMTREE: 
+class RANDOMTREE:
+   
+    """
+    This class is used to generate a Tree object, either randomly or by giving it an adjacency list. 
 
-    def __init__(self, height, max_noc, adj_list=None, edge_weights=True, colors=None):
+    Attributes
+    ----------
+    height : int
+        The height of the randomly generated Tree. 
+    max_noc : int
+        The maximum number of children for each node. 
+    adj_list : dict
+        An adjacency list to generate the Tree from. 
+    edge_weights : bool
+        If true, each edge will get (randomly generated) weights between 0 and 10. 
+        If false, the edges will be weighted equally with value 1. 
+    colors : int
+        The number of colors to use for the nodes. 
+
+    Methods
+    -------
+    get_number_of_nodes()
+        Return number of nodes in tree. 
+    get_number_of_colors()
+        Return number of colors used to color the nodes.  
+    get_dist_mat()
+        Return a matrix that contains the weighted distances between node i and j for each i and j.  
+    init_tree()
+        Helper function to initialize the tree from the adjacency matrix.
+    init_random_tree()
+        Helper function to initialize a randomly generated tree. 
+    create_random_tree()
+        Helper function to create a random tree as adjacency list. 
+    draw_tree()
+        Draw tree.
+
+    Examples
+    -------
+    Either create a RANDOMTREE object by giving it a height and the maximum number of children for each node 
+    or by giving it a adjacency matrix. You can also provide the adjacency matrix without colors. 
+    >>> random_tree = RANDOMTREE(height=4, max_noc=3, edge_weights=True, colors=2)
+    >>> adj_list = {0: {"children": [1], "color": 2},
+    ...             1: {"children": [2, 3], "color": 1}, 
+    ...             2: {"children": [], "color": 2}, 
+    ...             3: {"children": [], "color": 1}
+    ...             }
+    >>> random_tree = RANDOMTREE(adj_list=adj_list, edge_weights=True)
+
+    """
+
+    def __init__(self, height=1, max_noc=1, adj_list=None, edge_weights=True, colors=None):
         self.height = height
         self.max_noc = max_noc 
         self.adj_list = adj_list
         self.edge_weights = edge_weights
         self.colors = colors
-        if adj_list is None: 
-            self.Tree = self.get_random_tree()
+        if self.adj_list is None: 
+            self.Tree = self.init_random_tree()
         else: 
             self.Tree = self.init_tree()
-        if self.colors is not None: 
-            self.node_dict = self.color_tree()
-        else: 
-            self.node_dict = None
 
     def get_number_of_nodes(self): 
+        """
+        Get the number of nodes of the tree. 
+
+        Returns
+        -------
+            int: number of nodes in Tree 
+        """
         return len(self.Tree.nodes)
     
+    def get_color_dict(self): 
+
+        return {node[0]: node[1] for node in self.Tree.nodes.data("color")}
+    
+    def get_color_list(self): 
+
+        return list(set([node[1] for node in self.Tree.nodes.data("color")]))
+
     def get_number_of_colors(self):
+        """
+        Get the numbeer of colors used to color the tree. 
+
+        Returns
+        -------
+            int: number of colors
+        """
         
-        if self.colors is not None and self.adj_list is None: 
-            return self.colors
-        elif self.adj_list is not None: 
-            noc = 0
-            color_list = []
-            for node in self.node_dict.keys(): 
-                if self.node_dict[node]["color"] not in color_list: 
-                    noc = noc + 1
-                    color_list.append(self.node_dict[node]["color"])
-
-            return noc
-        else: 
-            return 1
-
-    def color_tree(self): 
-        node_dict = {}
-        for node in self.Tree.nodes.keys():
-            if self.adj_list is None: 
-                node_dict[node] = {"color": random.randint(1,self.colors)}
-            else: 
-                node_dict[node] = {"color": self.adj_list[node][1]}
-
-        return node_dict    
+        return len(self.get_color_list())
 
     def get_dist_mat(self): 
+        """
+        Return a matrix that contains the weighted distances between node i and j for each i and j.  
+
+        Returns
+        -------
+            np.array: weighted distances between nodes 
+        """
         
         apd = dict(nx.all_pairs_dijkstra(self.Tree, weight="weight"))
         dim = len(apd.keys())
@@ -58,40 +109,57 @@ class RANDOMTREE:
         for i in apd.keys(): 
             for j in apd[i][0].keys(): 
                 dist_mat[i][j] = apd[i][0][j]
-
+    
         return dist_mat
     
     def init_tree(self): 
+        """
+        Helper function to initialize the tree from the adjacency matrix.
 
+        Returns
+        -------
+            A tree using nx.Graph() from networx module
+
+        """
         tree_list = self.adj_list
         G = nx.Graph()
-        G.add_nodes_from(list(tree_list.keys()))
         for node in tree_list.keys():
-            if tree_list[node][0] != []: 
-                for child in tree_list[node][0]: 
+            G.add_node(node, color = tree_list[node].get("color", 1))
+            if tree_list[node]["children"] != []: 
+                for child in tree_list[node]["children"]: 
                     if self.edge_weights: 
-                        G.add_edge(node, child, weight=random.randint(1,10))
+                        G.add_edge(node, child, weight = random.randint(1,10))
                     else: 
-                        G.add_edge(node, child)
+                        G.add_edge(node, child, weight = 1)
 
         return G
 
+    def init_random_tree(self): 
+        """
+        Helper function to initialize a randomly generated tree with weights.
 
-    def get_random_tree(self): 
-        
+        Returns
+        -------
+            A tree using nx.Graph() from networx module
+
+        """
         tree_list = self.create_random_tree(self.height, self.max_noc)[0]
         G = nx.Graph()
-        G.add_nodes_from(list(tree_list.keys()))
         for node in tree_list.keys():
+            if self.colors is None: 
+                G.add_node(node, color = 1)
+            else: 
+                colors = min(len(tree_list.keys()), self.colors)
+                G.add_node(node, color = random.randint(1, colors))
             if tree_list[node] != []: 
                 for child in tree_list[node]: 
                     if self.edge_weights: 
-                        G.add_edge(node, child, weight=random.randint(1,10))
+                        G.add_edge(node, child, weight = random.randint(1,10))
                     else: 
-                        G.add_edge(node, child)
+                        G.add_edge(node, child, weight = 1)
 
         return G
-
+    
     def create_random_tree(self, height, max_noc): 
 
         if height < 0: 
@@ -125,20 +193,23 @@ class RANDOMTREE:
             return [tree, new_nodes]
     
     def draw_tree(self): 
-        
+        """
+        Draw tree. 
+        Returns
+        -------
+            None
+        """
         pos = nx.spring_layout(self.Tree)
     
-        if self.node_dict is not None: 
-            cols = [self.node_dict[node]["color"] for node in self.node_dict.keys()]
-            nx.draw_networkx_nodes(self.Tree, pos, node_size = 500, node_color = cols)
-        else: 
-            nx.draw_networkx_nodes(self.Tree, pos, node_size = 500)
+        
+        cols = [node[1] for node in self.Tree.nodes.data('color')]
+        nx.draw_networkx_nodes(self.Tree, pos, node_size = 500, node_color = cols)
         nx.draw_networkx_edges(self.Tree, pos, arrows=False, label="weight")
 
         # add labels to edges 
         if self.edge_weights: 
             edge_labels = {(u_e, v_e): e_weight for u_e, v_e, e_weight in self.Tree.edges.data('weight')}
-            nx.draw_networkx_edge_labels(self.Tree, pos, edge_labels= edge_labels)
+            nx.draw_networkx_edge_labels(self.Tree, pos, edge_labels = edge_labels)
 
         nx.draw_networkx_labels(self.Tree, pos)
         plt.show()
@@ -147,7 +218,14 @@ class RANDOMTREE:
 
 if __name__ == "__main__": 
 
-    random_tree = RANDOMTREE(2, 2, edge_weights=True, colors=2)
-    print(random_tree.node_dict)
-    random_tree.draw_tree()
+    adj_list = {0: {"children": [1], "color": 2},
+                1: {"children": [2, 3], "color": 1}, 
+                2: {"children": [], "color": 2}, 
+                3: {"children": [], "color": 1}
+                }
     
+    random_tree = RANDOMTREE(height=2, max_noc=2, edge_weights=False, colors=2)
+    print(random_tree.get_color_dict())
+    print(random_tree.Tree.nodes.data("color"))
+    random_tree.get_dist_mat() 
+    #random_tree.draw_tree()
--- src/solve_lin_prog.py
+++ src/solve_lin_prog.py
@@ -8,50 +8,77 @@ np.set_printoptions(threshold=sys.maxsize, linewidth=200)
 
 class SOlVELINPROG: 
 
-    def __init__(self, random_tree, k, r): 
+    def __init__(self, random_tree, k, alpha, beta): 
+        # TODO: Check if alpha and beta have correct length
         self.random_tree = random_tree
         self.k = k 
-        self.r = r
+        self.alpha = alpha
+        self.beta = beta
+        self.n = self.random_tree.get_number_of_nodes()
+        self.l = self.random_tree.get_number_of_colors()
+        self.dist_mat = self.random_tree.get_dist_mat()
+        self.color_list = self.random_tree.get_color_list()
+        self.color_dict = self.random_tree.get_color_dict()
+        
 
+    def c(self): 
 
-    def get_vector(self):
+        n = self.n 
+        c = np.ones((n+n*n,))
+        c[0:n] = np.zeros(n)
+        c[n:] = self.dist_mat.flatten()
+        
+        return c
 
-        n = self.random_tree.get_number_of_nodes()
-        l = self.random_tree.get_number_of_colors()
-        rows = 1 + n + 2*n*n + 2*n*l
+    def b(self):
+
+        n = self.n
+        l = self.l
+
+        #rows = 1 + n + 2*n*n + 2*n*l #### 1.) 
+        rows = 1 + n + n*n + 2*n*l   #### 2.) 
+         
 
         b = np.zeros((rows,))
 
+        # First condition
         b[0] = self.k 
 
+        # Second condition
         b[1:n+1] = -np.ones(n)
         
+        # Third condition
         b[n+1: n+1+n*n] = np.zeros(n*n)
 
-        b[n+1+n*n: n+1+2*n*n] = self.r * np.ones(n*n)
+        # Fourth condition
+        # r = ?
+        #b[n+1+n*n: n+1+2*n*n] = r * np.ones(n*n)
 
-        b[n+1+2*n*n: n+1+2*n*n+2*n*l] = np.zeros(2*n*l)
-
+        # Fifth condition
+        #b[n+1+2*n*n: n+1+2*n*n+2*n*l] = np.zeros(2*n*l)  #### 1.)
+        b[n+1+n*n: n+1+n*n+2*n*l] = np.zeros(2*n*l)       #### 2.)          
+        
         return b
         
 
-    def get_matrix(self, alpha, beta): 
+    def A(self): 
         
-        n = self.random_tree.get_number_of_nodes()
-        l = self.random_tree.get_number_of_colors()
-        dist_mat = self.random_tree.get_dist_mat()
-        node_dict = self.random_tree.node_dict
+        n = self.n
+        l = self.l 
+
         cols = n + n*n
-        rows = 1 + n + 2*n*n + 2*n*l
+        
+        #rows = 1 + n + 2*n*n + 2*n*l  #### 1.) 
+        rows = 1 + n + n*n + 2*n*l     #### 2.)
 
         A = np.zeros((rows, cols))
 
-        # First condition: sum(y) <= k
+        # First condition
         A[0, 0:n] = np.ones(n)
 
         # Second condition
-        for j in range(1,n+1): 
-            A[j, j*n:j*n+n] = -np.ones(n)
+        for j in range(n): 
+            A[j+1, (j+1)*n:(j+1)*n+n] = -np.ones(n)
 
         # Third condition
         for j in range(n):
@@ -60,63 +87,82 @@ class SOlVELINPROG:
                 A[n+1+i+n*j, n+i+n*j] = 1
 
         # Fourth condition
-        for j in range(n): 
-            for i in range(n):
+        #for j in range(n): 
+            #for i in range(n):
                 # A[1+n+n*n+i+4*j, n+i+4*j] = dist_mat[j,i]
                 #A[1+n+n*n+i+n*j, n+i+n*j] = dist_mat[j,i]
-                A[1+n+n*n+i+n*j, n+i+n*j] = 1
+                #A[1+n+n*n+i+n*j, n+i+n*j] = 1
 
         # Fifth condition
-        for c in range(l): 
-            for i in range(n): 
-                for j in range(n): 
-                    if node_dict[j]['color'] == c+1: 
-                        A[1+n+2*n*n+n*c+i, n+j*n+i] = 1-beta[c]
-                        #A[1+n+2*n*n+n*c+i, n+j*n+i] = 1
-                    else: 
-                        A[1+n+2*n*n+n*c+i, n+j*n+i] = -beta[c]
-            
-        
+        for c, color in enumerate(self.color_list): 
+            for i in self.color_dict.keys(): 
+                for j in self.color_dict.keys(): 
+                    if self.color_dict[j] == color: 
+                        A[1+n+n*n+n*c+i, n+j*n+i] = 1-self.beta[color]
+                    else:
+                        A[1+n+n*n+n*c+i, n+j*n+i] = -self.beta[color] 
+
+        for c, color in enumerate(self.color_list): 
+            for i in self.color_dict.keys(): 
+                for j in self.color_dict.keys(): 
+                    if self.color_dict[j] == color: 
+                        A[1+n+n*n+n*l+n*c+i, n+j*n+i] = self.alpha[color]-1
+                    else:
+                        A[1+n+n*n+n*l+n*c+i, n+j*n+i] = self.alpha[color] 
+                
         return A
     
     def solve_prog(self): 
 
-        alpha = [1/2, 1/2]
-        beta = [1/2, 1/2]
+        b_ub = self.b()
+        A_ub = self.A()
+        c_ = self.c()
+        
 
-        b = self.get_vector()
-        A = self.get_matrix(alpha, beta)
-        n = self.random_tree.get_number_of_nodes()
-        c = np.ones((n+n*n,))
-        c[0:n] = np.zeros(n)
+        res = linprog(c_, A_ub=A_ub, b_ub=b_ub)
+        
+        return res
 
-        res = linprog(c, A_ub=A, b_ub=b)
-        print(res.x)
+    def get_info(self, x): 
+
+        x = np.array(x)
+        x = x.reshape((int(len(x)/self.n),self.n))
+
+        for j in range(len(x[0])): 
+            if x[0][j] > 0: 
+                print(f"Node {j} is a center")
+
+        print("-----------------------------------------------------")
+
+        y = x[1:]
+        for j in range(y.shape[0]): 
+            for i in range(y.shape[1]):
+                if y[j][i] > 0: 
+                    print(f"Node {j} was assigned to center {i}")
+
+
 
 
 if __name__ == "__main__":
     
-    adj_list = {0: [[1, 2, 3, 4, 5, 6, 7], 1],
-                1: [[8, 9, 10, 11, 12, 13], 2], 
-                2: [[], 1], 
-                3: [[], 1], 
-                4: [[], 1], 
-                5: [[], 1], 
-                6: [[], 1], 
-                7: [[], 1],  
-                8: [[], 2], 
-                9: [[], 2], 
-                10: [[], 2], 
-                11: [[], 2], 
-                12: [[], 2], 
-                13: [[], 2]
+    adj_list = {0: {"children": [1, 2, 3, 4, 5, 6, 7], "color": 1},
+                1: {"children": [8, 9, 10, 11, 12, 13], "color": 2}, 
+                2: {"children": [], "color": 1}, 
+                3: {"children": [], "color": 1}, 
+                4: {"children": [], "color": 1}, 
+                5: {"children": [], "color": 1}, 
+                6: {"children": [], "color": 1}, 
+                7: {"children": [], "color": 1},  
+                8: {"children": [], "color": 2}, 
+                9: {"children": [], "color": 2}, 
+                10: {"children": [], "color": 2}, 
+                11: {"children": [], "color": 2 }, 
+                12: {"children": [], "color": 2}, 
+                13: {"children": [], "color": 2}
                 }
     
-    alpha = [1/2, 1/2]
-    beta = [1/2, 1/2]
-    random_tree = RANDOMTREE(1, 2, adj_list=adj_list, edge_weights=True, colors=2)
-    solve_lin_prog = SOlVELINPROG(random_tree, 2, 3)
-    #print(solve_lin_prog.get_matrix(alpha, beta))
-    #print(solve_lin_prog.get_vector())
-    solve_lin_prog.solve_prog()
+
+    random_tree = RANDOMTREE(adj_list=adj_list, edge_weights=True)
+    solve_lin_prog = SOlVELINPROG(random_tree, 2, alpha = {1: 1/2, 2:1/2}, beta = {1: 1/2, 2:1/2})
+    solve_lin_prog.get_info(solve_lin_prog.solve_prog().x)
     random_tree.draw_tree()
