0447e6fff2dcbc8cd19fcce113217604ba81f8a0
Kilian Schnappinger <schnappingerkilian@gmail.com>
2023-10-17 17:16:31 +0200

    added more unittests

--- tests/test_random_tree.py
+++ tests/test_random_tree.py
@@ -33,6 +33,15 @@ class TestClass(unittest.TestCase):
         self.assertLessEqual(rt.get_number_of_colors(), 3)
         self.assertGreaterEqual(rt.get_number_of_colors(), 1)
 
+        # With random weights and colors
+        rt = RANDOMTREE(adj_list=adj_list, max_dist=5, colors=2)
+        self.assertEqual(rt.get_number_of_nodes(), 4)
+        self.assertLessEqual(len(rt.get_color_list()), 2)
+        max_weight = max([e_weight for u_e, v_e, e_weight in rt.Tree.edges.data('weight')])
+        self.assertLessEqual(max_weight, 5)
+        self.assertGreaterEqual(max_weight, 1)
+
+        # With deterministic weights and colors
         for node in adj_list.keys(): 
             if node%2 == 0: 
                 adj_list[node]["color"] = 1
@@ -40,6 +49,15 @@ class TestClass(unittest.TestCase):
                 adj_list[node]["color"] = 2
         rt = RANDOMTREE(adj_list=adj_list)
         self.assertEqual(rt.get_number_of_colors(), 2)
+
+        ### Init Random Tree
+        # Without colors and weights
+        rt = RANDOMTREE(5,3)
+        self.assertLessEqual(rt.get_number_of_nodes(), 364)
+        self.assertGreaterEqual(rt.get_number_of_nodes(), 6)   
+        self.assertEqual(rt.get_number_of_colors(), 1) 
+        max_weight = max([e_weight for u_e, v_e, e_weight in rt.Tree.edges.data('weight')]) 
+        self.assertEqual(max_weight, 1)   
 
 
 
